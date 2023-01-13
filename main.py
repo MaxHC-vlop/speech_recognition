c68f75e9705f701b1e26477427ea7ce874a13581
Vladuslav Dragushin <maxhc@inbox.ru>
2023-01-13 18:25:35 +0300

    setup DialogFlow

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
 import logging
+import os
 
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from dialogflow import detect_intent_texts
+
+from telegram import Update
+from telegram.ext import Updater, CommandHandler
+from telegram.ext import Filters, CallbackContext
+from telegram.ext import MessageHandler
 from environs import Env
 
-# Enable logging
+
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
 )
 
 logger = logging.getLogger(__name__)
 
 
 def start(update: Update, context: CallbackContext) -> None:
-    user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Здравствуй {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
-    )
+    username = update.message.from_user.username
+    message = f'Здравствуй, {username}'
+
+    update.message.reply_text(message)
 
 
-def help_command(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Help!')
+def send_dialogflow_message(update: Update, context: CallbackContext) -> None:
+    session_id = update.message.from_user.id
+    dialogflow_response = detect_intent_texts(
+        os.getenv('DIALOD_ID'),
+        session_id,
+        [update.message.text], 'ru')
 
-
-def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+    update.message.reply_text(dialogflow_response)
 
 
 def main() -> None:
@@ -38,9 +45,9 @@ def main() -> None:
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(
+        MessageHandler(Filters.text, send_dialogflow_message)
+    )
 
     updater.start_polling()
 
